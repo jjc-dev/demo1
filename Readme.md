8b8af81d83040c98e816d11741cbb18365d55ac7
Jonathan <Jonathan@raptor>
2020-04-27 06:46:49 +0100

    initial commit of demo project

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# Simple Spring Boot Application
+
+Basic Spring Boot Java Web application providing REST GET interface.
+
+## Test Interfaces
+REST GET (provided here as localhost, but replace with appropriate URL based on deployment):
+
+* `http://localhost:5000` provides version information in JSON format
+* `http://localhost:5000/hello` provides a text response with 'Hello World' and the version number.
---
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -0,0 +1,31 @@
+package com.example.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class HomeController {
+    @Value("${app.version}")
+    private String appVersion;
+
+
+    @GetMapping
+    @RequestMapping("/")
+    public Map<String, String> getStatus() {
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("app-version", appVersion);
+        return map;
+    }
+
+    @GetMapping
+    @RequestMapping("/hello")
+    public String getHello() {
+        return "Hello World: " + appVersion;
+    }
+
+}
