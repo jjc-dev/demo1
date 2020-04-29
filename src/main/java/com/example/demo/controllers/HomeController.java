7139b945b5c9640c1d86346f73303d701409dcd4
Jonathan <Jonathan@raptor>
2020-04-29 13:23:02 +0100

    small code changes

--- src/main/java/com/example/demo/controllers/HomeController.java
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -1,31 +1,35 @@
 package com.example.demo.controllers;
 
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @RestController
 public class HomeController {
     @Value("${app.version}")
     private String appVersion;
-
-
+    private Locale locale = new Locale("en", "GB");
+    DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.DEFAULT, locale);
+    
     @GetMapping
     @RequestMapping("/")
     public Map<String, String> getStatus() {
         Map<String, String> map = new HashMap<String, String>();
-        map.put("Openshift app-version", appVersion + "!");
+        map.put("The Openshift app-version", appVersion + " " + dateFormat.format(new Date()));
         return map;
     }
 
     @GetMapping
     @RequestMapping("/hello")
     public String getHello() {
-        return "Hello Openshift World: " + appVersion + "!";
+        return "Hello Openshift World: " + appVersion + " "  + dateFormat.format(new Date());
     }
 
 }
